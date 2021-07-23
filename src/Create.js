5b669677ee9b06a95840e62b84c41dfad8848670
mikey084 <lium039@gmail.com>
2021-07-23 13:47:18 -0700

    Add react router + fix dom umount fetch error

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>This is Create</h2>
+        </div>
+    )
+}
+
+export default Create;
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,33 +1,36 @@
-import {useState, useEffect} from 'react';
-
+import { useState, useEffect } from 'react';
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-    useEffect(() => {
-      setTimeout(() => {
-        fetch(url)
-        .then((res) => {
-          console.log(res);
-          if (!res.ok){
-            throw Error("could not fetch data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const abortCont = new AbortController();
+    setTimeout(() => {
+      fetch(url, {signal: abortCont.signal})
+      .then(res => {
+        if (!res.ok) { // server error
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {  //connection error
+        if (err.name === "AbortError"){
+          console.log("Fetch aborted");
+        } else{
           setIsPending(false);
-          setError(null);
-  
-        })
-        .catch((err) => {
           setError(err.message);
-          setIsPending(false);
-        })
-      }, 1000)
-    }, [url]);
+        }
+      })
+    }, 1000);
+    return () => abortCont.abort();
+  },)
 
-    return  {data, isPending, error};
+  return { data, isPending, error };
 }
-
+ 
 export default useFetch;
